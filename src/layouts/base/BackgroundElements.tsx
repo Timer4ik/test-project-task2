5a438b680dd385d786adb6807a7d7cc240165688
timer4ik <timergalievadel@yandex.ru>
2023-07-19 08:55:53 +0300

    fix

--- src/layouts/base/BackgroundElements.tsx
+++ src/layouts/base/BackgroundElements.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 const BackgroundElements = () => {
     return (
         <>
-            <div className="purple-smooth-ball" />
-            <div className="purple-light" />
-            <div className="red-ball" />
-            <div className="red-light" />
-            <div className="red-smooth-ball" />
+            <img src='/img/purple-ball.svg' className="purple-smooth-ball" />
+            <img src='/img/purple-light.svg' className="purple-light" />
+            <img src='/img/red-ball.svg' className="red-ball" />
+            <img src='/img/red-light.svg' className="red-light" />
+            <img src='/img/red-ball.svg' className="red-smooth-ball" />
         </>
     )
 }
